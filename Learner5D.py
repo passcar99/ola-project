44a4945dd55ad96f7d00c3979da208e28c39ff4b
Daxdop <104645665+Daxdop@users.noreply.github.com>
2022-05-27 18:48:22 +0200

    Learner5D class + LearnerTS5D

---
+++ Learner5D.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Learner5D:
+    def __init__(self,n_arms):
+        self.n_arms=n_arms#per product
+        self.t=0
+        self.rewards_per_arm= x = [[]for i in range(n_arms**5)]
+        #is the matrix spiattellata in a vector: M[i,j,k,l,m] identify the multiarm
+        #with i-th budget on first product, j-th on second, k-th on third and so on
+        #the mapping from matrix to vector is M[i,j,k,l,m]=v[i+n_arms*j+n_arms^2*k+n_arms^3*l+n_arms^4*m]
+        self.collected_rewards=np.array([])
+
+    def update_observations(self,pulled_arm,reward):#!Attention Reward must include also the eventual budget not used
+        #pulled_arm vector of length 5, contains the arm pulled for each product
+        i=pulled_arm[0];
+        j=pulled_arm[1];
+        k=pulled_arm[2];
+        l=pulled_arm[3];
+        m=pulled_arm[4];
+        
+        self.rewards_per_arm[i+self.n_arms*j+self.n_arms^2*k+self.n_arms^3*l+self.n_arms^4*m].append(reward)
+        self.collected_rewards=np.append(self.collected_rewards,reward)
---
+++ TS_Learner5D.py
@@ -0,0 +1,25 @@
+from Learner5D import *
+
+class TS_Learner5D(Learner5D):
+
+    def __init__(self,n_arms):
+        super().__init__(n_arms)
+        self.beta_parameters=np.ones((n_arms,n_arms,n_arms,n_arms,n_arms,2))
+
+    def pull_arm(self,constraint_mask):# constraint_mask a list of tuple of length 5 which identify the unavailable multi_arms
+        beta_outcome=np.random.beta(self.beta_parameters[:,:,:,:,:,0],self.beta_parameters[:,:,:,:,:,1])
+        for t in constraint_mask:
+            beta_outcome[t[0],t[1],t[2],t[3],t[4]]=-100000;
+        idx=np.unravel_index(np.argmax(beta_outcome),beta_outcome.shape)
+        return idx#tuple of the 5 inndexes of the pulled multi arm
+
+    def update(self,pulled_arm,reward):#!Attention Reward must include also the eventual budget not used
+        self.t+=1
+        self.update_observations(pulled_arm,reward)
+        i=pulled_arm[0];
+        j=pulled_arm[1];
+        k=pulled_arm[2];
+        l=pulled_arm[3];
+        m=pulled_arm[4];
+        self.beta_parameters[i,j,k,l,m,0]=self.beta_parameters[i,j,k,l,m,0]+reward
+        self.beta_parameters[i,j,k,l,m,1]=self.beta_parameters[i,j,k,l,m,1]+1.0-reward
