726990ba16022ca7ebe5f2cceb1444a73c037c7c
Daxdop <davide.grisi@mail.polimi.it>
2022-06-30 11:24:21 +0200

    TestGeneral modifications

--- TestGeneral.py
+++ TestGeneral.py
@@ -7,32 +7,40 @@ import matplotlib.pyplot as plt
 import sys
 
 
-Prob_Buy=np.array([1,0.5,1,1,1])
-Con_matrix=np.array([[0,0.5,0.3,0,0],[0,0,0,0.5,0.3],[0.3,0,0,0,0.5],[0.5,0.3,0,0,0],[0.3,0.5,0,0,0]])
+Prob_Buy=prob_buy = np.array([0.5, 0.2, 0.5, 0.7, 0.7])#np.array([1,0.5,1,1,1])
+Con_matrix=np.array([[0, 0.2, 0.4, 0.3, 0.1],
+                                    [0.5, 0, 0.1, 0.3, 0.1],
+                                    [0.3, 0.2, 0, 0.1, 0.4],
+                                    [0.13, 0.17, 0.30, 0, 0.4],
+                                    [0.16, 0.34, 0.15, 0.25, 0],
+                                    ])#np.array([[0,0.5,0.3,0,0],[0,0,0,0.5,0.3],[0.3,0,0,0,0.5],[0.5,0.3,0,0,0],[0.3,0.5,0,0,0]])
 conpam_matrix = [
         {"alpha_params": [(0, 10, 20), (2, 15, 20),(2, 20, 20),(2, 15, 20),(1, 15, 20)], 
         "features":[0, 0], "total_mass":80, "avg_number":100}, 
                     ]
-env=Environment(conpam_matrix,Con_matrix,Prob_Buy,[1,1,1,2,0],[1,1,1,1,1])
+avg_sold = [6,10,5,5,6]
+margins = [30, 20, 30, 40, 50]
+env=Environment(conpam_matrix,Con_matrix,Prob_Buy,avg_sold,margins)
 #FOR DEBUG PURPOSES THIS BINARY CASE CAN BE EASILY REPRESENTED IN 1D PASSING FROM BINARY TO INTEGER AND PLOTTING CUMULATIVE DATA AND
 #EVENTUALLY THE BETA DISTRIBUTION
-arms_budgets=np.array([0,10])#([[0,0.25,0.5,0.75,1],[0,0.25,0.5,0.75,1],[0,0.25,0.5,0.75,1],[0,0.25,0.5,0.75,1],[0,0.25,0.5,0.75,1]])
+arms_budgets=np.array([5, 10])#np.array([5,10])#([[0,0.25,0.5,0.75,1],[0,0.25,0.5,0.75,1],[0,0.25,0.5,0.75,1],[0,0.25,0.5,0.75,1],[0,0.25,0.5,0.75,1]])
 n_arms=len(arms_budgets)
 n_super_arms=pow(n_arms,5)
 
 Result=[]
-
+ii=0
 for m in range(n_arms):
     for l in range(n_arms):
         for k in range(n_arms):
             for j in range(n_arms):
                 for i in range(n_arms):
+                    sys.stdout.write('\r')
                     res=[0,arms_budgets[i],arms_budgets[j],arms_budgets[k],arms_budgets[l],arms_budgets[m]]
-                    DirPar=conpam_matrix[0]+res
-                    ParSum=np.sum(DirPar)
-                    alpha_mean = [i/ParSum for i in DirPar]
-                    res[0]=env.round(alpha_mean)
+                    res[0]=env.pull_arm_excpected_value(res[1:])
                     Result.append(res)
+                    sys.stdout.write("{:.1f}%".format((100/(n_super_arms-1)*ii)))
+                    sys.stdout.flush()
+                    ii+=1
 
 Clayrvoiant=np.zeros(n_super_arms);
 for i in range(n_super_arms):
@@ -40,33 +48,16 @@ for i in range(n_super_arms):
 print("------------------------------------------")
 Cumulative=np.zeros(n_super_arms);
 LastCum=np.zeros(n_super_arms);
-Nn=300;
+Nn=10;
 Regret=np.zeros(Nn+1);
-# TS 
-#learner=TS_Learner5D(n_arms)
-#for i in range(Nn):
-#    multi_arm_idx=learner.pull_arm([])
-#    budget=[arms_budgets[0][multi_arm_idx[0]],arms_budgets[1][multi_arm_idx[1]],arms_budgets[2][multi_arm_idx[2]],arms_budgets[3][multi_arm_idx[3]],arms_budgets[4][multi_arm_idx[4]]]
-    #budget.reverse()
-    #Number=int("".join(str(i) for i in budget),2)
-    #budget.reverse()
-    
-#    Number=multi_arm_idx[0]+n_arms*multi_arm_idx[1]+pow(n_arms,2)*multi_arm_idx[2]+pow(n_arms,3)*multi_arm_idx[3]+pow(n_arms,4)*multi_arm_idx[4]
-#    Cumulative[Number]=Cumulative[Number]+1
-#    reward=env.pull_arm(budget)
-#    learner.update(multi_arm_idx,reward)
-#    if i>(Nn-100):
-#        LastCum[Number]=LastCum[Number]+1
-        
 
-#plt.plot(Cumulative/Nn, label="TS % pulled")
-
-learner=GPTS_Learner5D(n_arms,arms_budgets/max(arms_budgets));
+learner=GPTS_Learner5D(n_arms,arms_budgets/max(arms_budgets))
+#NORMALIZATION, IS ALSO A SHIFT NEEDED??
 
 for i in range(Nn):
     sys.stdout.write('\r')
     multi_arm_idx=learner.pull_arm([])
-    budget=[arms_budgets[multi_arm_idx[0]],arms_budgets[multi_arm_idx[1]],arms_budgets[multi_arm_idx[2]],arms_budgets[multi_arm_idx],arms_budgets[multi_arm_idx[4]]]
+    budget=[arms_budgets[multi_arm_idx[0]],arms_budgets[multi_arm_idx[1]],arms_budgets[multi_arm_idx[2]],arms_budgets[multi_arm_idx[3]],arms_budgets[multi_arm_idx[4]]]
     reward=env.pull_arm(budget)
     Number=multi_arm_idx[0]+n_arms*multi_arm_idx[1]+pow(n_arms,2)*multi_arm_idx[2]+pow(n_arms,3)*multi_arm_idx[3]+pow(n_arms,4)*multi_arm_idx[4]
     #Regret[i+1]=Regret[i]+max(Clayrvoiant)-reward actual for the leatner mean for the clayrvoiant
@@ -79,7 +70,7 @@ for i in range(Nn):
 CumFast=np.array([ len(listElem) for listElem in learner.rewards_per_arm])
 plt.figure(1)
 plt.plot(Clayrvoiant, label="Expected Value")
-plt.plot(CumFast/Nn, label="GPTS % pulled FAST")
+plt.plot(CumFast/Nn*max(Clayrvoiant), label="GPTS % pulled FAST")
 plt.legend(loc="upper left")
 plt.show()
 
