c3cdec104a07e9a58b34b51cc4c91d4f83b7408b
passcar99 <pasquale.scarmozzino99@gmail.com>
2022-07-07 13:22:43 +0200

    Step 5 finished

--- learners/GPUCB_Learner5.py
+++ learners/GPUCB_Learner5.py
@@ -5,50 +5,52 @@ from .GPUCB_Learner import GPUCB_Learner
 
 class GPUCB_Learner5(GPUCB_Learner):
 
-    def __init__(self, arms, conpam_matrix:List[Dict], prob_buy, avg_sold,margins, bounds,environment_type = 'fast'):
+    def __init__(self, arms, conpam_matrix:List[Dict], conn_constr_matrix, prob_buy,avg_sold, margins, bounds,environment_type = 'fast'):
         """ 
         :param arms: list of arms (budgets).
         :param conpam_matrix: data about the environment (see the environment classes).
-        :param con_matrix: connectivity matrix of the graph.
+        :param conn_constr_matrix: connectivity mask of the graph.
         :param prob_buy: probability that an item is bought when displayed as primary.
         :param margins: margin (profit) for each arm.
         :param bounds: lower and upper bounds for each product (n_products*2 matrix).
         :param environment_type: type of environment to use to estimate the expected margin.
         """
-        self.con_matrix = np.ones((len(margins), len(margins)))
-        self.transition_count_matrix = np.zeros((len(margins), len(margins)))
-        self.prod_count = np.zeros((len(margins)))
-        super().__init__(arms,conpam_matrix,self.con_matrix, prob_buy, avg_sold,margins, bounds, environment_type)
+        self.constraints_matrix = conn_constr_matrix
+        self.con_matrix = self.constraints_matrix * 1.0
+        self.credit_matrix = np.zeros((len(margins), len(margins)))
+        self.prod_count = np.zeros((len(margins), len(margins)))
+        super().__init__(arms,conpam_matrix,self.con_matrix, prob_buy, avg_sold, margins, bounds, environment_type)
 
             
 
-    def update_model(self,activation_history):
+    def update_model(self, items_sold, activation_history):
         """ 
         Update the Gaussian Processes for every product to incorporate the new data.
         Update the average number of items sold.
         Update the estimate on the graph weights.
         """
         super().update_model()
-        self.prod_count += (activation_history!=0).sum(axis=0)
+
         for visit in range(activation_history.shape[0]):
             visit_prob = np.zeros((self.n_products, self.n_products))
+            active_nodes = np.logical_and(activation_history[visit]!=0, items_sold[visit]!=0) > 0
             for product in range(self.n_products):
                 activ_time = activation_history[visit, product]
-                previous_nodes = np.logical_and(activation_history[visit] == activ_time-1, activation_history[visit]!=0)
+                previous_nodes = np.logical_and(activation_history[visit] == activ_time-1,active_nodes)
                 sum_weights = previous_nodes.sum()
-                if sum_weights!=0:
-                    visit_prob[previous_nodes, product] = 1/sum_weights
-            self.transition_count_matrix += visit_prob
-        self.con_matrix = self.transition_count_matrix/self.prod_count
-        self.con_matrix[np.isnan(self.con_matrix)] = 0 # set to zero items with zero visits
-
+                if sum_weights != 0:
+                    visit_prob[previous_nodes, product] = (1/sum_weights)#/self.prob_buy[previous_nodes]
+                other_prods = np.arange(self.n_products)!=product
+                self.prod_count[other_prods, product] += (previous_nodes)[other_prods]>0 # active before current node
+                self.prod_count[other_prods, product] += (np.logical_and(active_nodes, activ_time==0))[other_prods] # active but current node isn't
+            self.credit_matrix += visit_prob
+        non_zero_prods = np.logical_and((self.prod_count!=0), self.constraints_matrix!=0)
+        self.con_matrix[non_zero_prods] = self.credit_matrix[non_zero_prods]/self.prod_count[non_zero_prods]
         self.env.con_matrix = self.con_matrix
                 
-        
-
     def update(self, pulled_arm, reward):
         self.t += 1
         self.update_observations(pulled_arm, reward)
-        self.update_model(reward['activation_history'])
-        
+        self.update_model(reward['items'],reward['activation_history'])
+
 
--- learners/TSLearner5.py
+++ learners/TSLearner5.py
@@ -9,20 +9,21 @@ class GPTS_Learner5(GPTS_Learner):
     """ 
     Gaussian Process Thompson Sampling bandit. It estimated the click rate(?) of each product independently.
     At every round it computes the alphas and the expected margins and fills the DP table. Then selects one of the 
-    feasible superarm. The average number of items sold is estimated according to data
+    feasible superarm. The transition weights are estimated according to data.
     
     """
-    def __init__(self, arms, conpam_matrix:List[Dict], prob_buy,avg_sold, margins, bounds,environment_type = 'fast'):
+    def __init__(self, arms, conpam_matrix:List[Dict], conn_constr_matrix, prob_buy,avg_sold, margins, bounds,environment_type = 'fast'):
         """ 
         :param arms: list of arms (budgets).
         :param conpam_matrix: data about the environment (see the environment classes).
-        :param con_matrix: connectivity matrix of the graph.
+        :param conn_constr_matrix: connectivity mask of the graph.
         :param prob_buy: probability that an item is bought when displayed as primary.
         :param margins: margin (profit) for each arm.
         :param bounds: lower and upper bounds for each product (n_products*2 matrix).
         :param environment_type: type of environment to use to estimate the expected margin.
         """
-        self.con_matrix = np.ones((len(margins), len(margins)))
+        self.constraints_matrix = conn_constr_matrix
+        self.con_matrix = self.constraints_matrix * 1.0
         self.credit_matrix = np.zeros((len(margins), len(margins)))
         self.prod_count = np.zeros((len(margins), len(margins)))
         super().__init__(arms,conpam_matrix,self.con_matrix, prob_buy, avg_sold, margins, bounds, environment_type)
@@ -36,19 +37,22 @@ class GPTS_Learner5(GPTS_Learner):
         Update the estimate on the graph weights.
         """
         super().update_model()
-        self.prod_count += (activation_history>0).sum(axis=0)
+
         for visit in range(activation_history.shape[0]):
             visit_prob = np.zeros((self.n_products, self.n_products))
+            active_nodes = np.logical_and(activation_history[visit]!=0, items_sold[visit]!=0) > 0
             for product in range(self.n_products):
                 activ_time = activation_history[visit, product]
-                previous_nodes = np.logical_and(activation_history[visit] == activ_time-1,np.logical_and(activation_history[visit]!=0, items_sold[visit]!=0))
+                previous_nodes = np.logical_and(activation_history[visit] == activ_time-1,active_nodes)
                 sum_weights = previous_nodes.sum()
                 if sum_weights != 0:
                     visit_prob[previous_nodes, product] = (1/sum_weights)#/self.prob_buy[previous_nodes]
+                other_prods = np.arange(self.n_products)!=product
+                self.prod_count[other_prods, product] += (previous_nodes)[other_prods]>0 # active before current node
+                self.prod_count[other_prods, product] += (np.logical_and(active_nodes, activ_time==0))[other_prods] # active but current node isn't
             self.credit_matrix += visit_prob
-        non_zero_prods = (self.prod_count!=0)
-        self.con_matrix[:, non_zero_prods] = self.credit_matrix[:, non_zero_prods]/self.prod_count[non_zero_prods]
-        #self.con_matrix[np.isnan(self.con_matrix)] = 0 # put to zero items with zero visits
+        non_zero_prods = np.logical_and((self.prod_count!=0), self.constraints_matrix!=0)
+        self.con_matrix[non_zero_prods] = self.credit_matrix[non_zero_prods]/self.prod_count[non_zero_prods]
         self.env.con_matrix = self.con_matrix
                 
         
