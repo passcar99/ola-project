28a4731f5f34e8a48f77f439a61b4a782b60b9c7
Daxdop <davide.grisi@mail.polimi.it>
2022-07-12 10:48:07 +0200

    feat: LipsConstant Calculation

--- RewardLipschitzConstantApproximation.py
+++ RewardLipschitzConstantApproximation.py
@@ -19,7 +19,11 @@ if __name__ == '__main__':
     #bounds = np.array([[5, 100],[0, 80],[0, 50],[20, 100],[0, 100]])
     bounds = np.array([[2, 100],[2, 100],[-1, 100],[2, 100],[-1, 100]])
 
-    env = RandomEnvironment(conpam_matrix, connectivity_matrix, prob_buy, avg_sold, margins)
+    conpam_matrix = [
+        {"alpha_params": [(20, 30, 20), (2, 15, 20),(2, 20, 20),(2, 15, 20),(1, 15, 20)], 
+        "features":[0, 0], "total_mass":100, "avg_number":100}, 
+                    ]
+
     
     n_products = len(connectivity_matrix)
     n_arms = len(arms)
@@ -57,9 +61,60 @@ if __name__ == '__main__':
                         arms_list=list(arms)
                         super_arm_idx=[arms_list.index(arms[i]),arms_list.index(arms[j]),arms_list.index(arms[k]),arms_list.index(arms[l]),arms_list.index(arms[m]),]
                         if super_arm_idx in unfeasible_arms:
-                            Clayrvoiant[ii]=-np.inf
+                            Interpolation[i,j,k,l,m]=-np.inf
                         else:
-                            Clayrvoiant[ii]=envEx.pull_arm_excpected_value(super_arm,1)-sum(super_arm)
-                        sys.stdout.write("Clayrvoiant: {:.1f}%".format((100/(pow(n_arms,5)-1)*ii)))
+                            Interpolation[i,j,k,l,m]=envEx.pull_arm_excpected_value(super_arm,1)-sum(super_arm)
+                        sys.stdout.write("Reward Calculation: {:.1f}%".format((100/(pow(n_arms,5)-1)*ii)))
                         sys.stdout.flush()
                         ii+=1
+
+    ii=0
+    Lips=0
+    for m in range(n_arms):
+        for l in range(n_arms):
+            for k in range(n_arms):
+                for j in range(n_arms):
+                    for i in range(n_arms):
+                        LocalLips=0
+                        sys.stdout.write('\r')
+                        super_arm=[arms[i],arms[j],arms[k],arms[l],arms[m]]
+                        arms_list=list(arms)
+                        super_arm_idx=[arms_list.index(arms[i]),arms_list.index(arms[j]),arms_list.index(arms[k]),arms_list.index(arms[l]),arms_list.index(arms[m]),]
+                        if super_arm_idx in unfeasible_arms:
+                            LocalLips=0
+                            Local_i_p=0
+                            Local_i_n=0
+                            Local_j_p=0
+                            Local_j_n=0
+                            Local_k_p=0
+                            Local_k_n=0
+                            Local_l_p=0
+                            Local_l_n=0
+                            Local_m_p=0
+                            Local_m_n=0
+                        else:
+                            Local_i_p=0 if [i+1,j,k,l,m] in unfeasible_arms else abs(Interpolation[i+1 if i+1<n_arms else i, j,k,l,m]-Interpolation[i, j,k,l,m])/abs(arms[i+1]-arms[i])
+                            Local_i_n=0 if [i-1,j,k,l,m] in unfeasible_arms else abs(Interpolation[i-1 if i-1>=0 else i, j,k,l,m]-Interpolation[i, j,k,l,m])/abs(arms[i-1]-arms[i])
+                            
+                            Local_j_p=0 if [i,j+1,k,l,m] in unfeasible_arms else abs(Interpolation[i,j+1 if j+1<n_arms else  j,k,l,m]-Interpolation[i, j,k,l,m])/abs(arms[j+1]-arms[j])
+                            Local_j_n=0 if [i,j-1,k,l,m] in unfeasible_arms else abs(Interpolation[i,j-1 if j-1>=0 else  j,k,l,m]-Interpolation[i, j,k,l,m])/abs(arms[j-1]-arms[j])
+
+                            Local_k_p=0 if [i,j,k+1,l,m] in unfeasible_arms else abs(Interpolation[i,j,k+1 if k+1<n_arms else k,l,m]-Interpolation[i, j,k,l,m])/abs(arms[k+1]-arms[k])
+                            Local_k_n=0 if [i,j,k-1,l,m] in unfeasible_arms else abs(Interpolation[i,j,k-1 if k-1>=0 else  k,l,m]-Interpolation[i, j,k,l,m])/abs(arms[k-1]-arms[k])
+
+                            Local_l_p=0 if [i,j,k,l+1,m] in unfeasible_arms else abs(Interpolation[i,j,k,l+1 if l+1<n_arms else l,m]-Interpolation[i, j,k,l,m])/abs(arms[l+1]-arms[l])
+                            Local_l_n=0 if [i,j,k,l-1,m] in unfeasible_arms else abs(Interpolation[i,j,k,l-1 if l-1>=0 else  l,m]-Interpolation[i, j,k,l,m])/abs(arms[l-1]-arms[l])
+
+                            Local_m_p=0 if [i,j,k,l,m+1] in unfeasible_arms else abs(Interpolation[i,j,k,l,m+1 if m+1<n_arms else m]-Interpolation[i, j,k,l,m])/abs(arms[m+1]-arms[m])
+                            Local_m_n=0 if [i,j,k,l,m-1] in unfeasible_arms else abs(Interpolation[i,j,k,l,m-1 if m-1>=0 else  m]-Interpolation[i, j,k,l,m])/abs(arms[m-1]-arms[m])
+
+                            
+                        LocalLips=max(Local_i_p,Local_i_n,Local_j_p,Local_j_n,Local_k_p,Local_k_n,Local_l_p,Local_l_n,Local_m_p,Local_m_n)
+                        if LocalLips==np.inf:
+                            print("erroe")
+                        Lips=max(Lips,LocalLips)
+                        sys.stdout.write("Lipschitz Calculation: {:.1f}%".format((100/(pow(n_arms,5)-1)*ii)))
+                        sys.stdout.flush()
+                        ii+=1
+    
+    print('Value: '+str(Lips))
