6e6d90be2f854a46a3a7919786fcacef3a223f8a
Daxdop <davide.grisi@mail.polimi.it>
2022-07-16 19:55:43 +0200

    READY FOR SIMULATION

--- Step4Test.py
+++ Step4Test.py
@@ -46,9 +46,9 @@ if __name__ == '__main__':
 
     clairvoyant_rewards_per_experiment = []
 
-    n_experiments = 5
+    n_experiments = 15
 
-    T = 100
+    T = 160
 
 
     for e in tqdm(range(n_experiments)):
